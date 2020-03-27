aedf8ac50e530f87cf6c7420c905e73f1d990ef9
thesomeexp <thesomeexp@riseup.net>
2020-03-27 16:13:56 +0800

    20200327

--- src/main/java/com/someexp/parking_info/config/WebMvcConfigurer.java
+++ src/main/java/com/someexp/parking_info/config/WebMvcConfigurer.java
@@ -6,7 +6,10 @@ import com.someexp.parking_info.interceptor.RAInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.io.File;
 
 @Configuration
 public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
